ac06454cb21a2103960458b6a2e4768618e340a1
xhayper <hayper1919@gmail.com>
2021-10-23 11:25:37 +0700

    Added another check ig lol

--- src/main/java/com/changedmc/turned/event/EventCapability.java
+++ src/main/java/com/changedmc/turned/event/EventCapability.java
@@ -1,6 +1,7 @@
 package com.changedmc.turned.event;
 
 import com.changedmc.turned.capability.transfur.TransfurCapabilityProvider;
+import com.changedmc.turned.entity.latex.LatexEntity;
 import com.changedmc.turned.util.Reference;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -11,10 +12,9 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class EventCapability {
     @SubscribeEvent
     public static void onAttachCapabilitiesEvent(AttachCapabilitiesEvent<Entity> event) {
-        if (event.getObject() instanceof PlayerEntity) {
-            TransfurCapabilityProvider provider = new TransfurCapabilityProvider();
-            event.addCapability(new ResourceLocation(Reference.MOD_ID, "transfur"), provider);
-            event.addListener(provider::invalidate);
-        }
+        if (!((event.getObject() instanceof PlayerEntity) || (event.getObject() instanceof LatexEntity))) return;
+        TransfurCapabilityProvider provider = new TransfurCapabilityProvider();
+        event.addCapability(new ResourceLocation(Reference.MOD_ID, "transfur"), provider);
+        event.addListener(provider::invalidate);
     }
 }
