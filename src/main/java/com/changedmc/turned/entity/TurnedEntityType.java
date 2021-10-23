323d660246c0a0b2d083a5910a4579fefeabd751
xhayper <hayper1919@gmail.com>
2021-10-23 15:55:27 +0700

    Fixed Hitbox size and make LatexEntity and make DLME and DLSLE extends LatexEntity

--- src/main/java/com/changedmc/turned/entity/TurnedEntityType.java
+++ src/main/java/com/changedmc/turned/entity/TurnedEntityType.java
@@ -13,8 +13,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class TurnedEntityType {
     public static DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, Reference.MOD_ID);
 
-    public static final RegistryObject<EntityType<DarkLatexMaleEntity>> DARK_LATEX_MALE = ENTITY_TYPES.register("dark_latex_male", () -> EntityType.Builder.of(DarkLatexMaleEntity::new, EntityClassification.CREATURE).sized(1.0f, 1.0f).build(new ResourceLocation(Reference.MOD_ID, "dark_latex_male").toString()));
+    public static final RegistryObject<EntityType<DarkLatexMaleEntity>> DARK_LATEX_MALE = ENTITY_TYPES.register("dark_latex_male", () -> EntityType.Builder.of(DarkLatexMaleEntity::new, EntityClassification.CREATURE).sized(1.0f, 2.0f).build(new ResourceLocation(Reference.MOD_ID, "dark_latex_male").toString()));
 
 
-    public static final RegistryObject<EntityType<DarkLatexSnowLeopardEntity>> DARK_LATEX_SNOW_LEOPARD = ENTITY_TYPES.register("dark_latex_snow_leopard", () -> EntityType.Builder.of(DarkLatexSnowLeopardEntity::new, EntityClassification.CREATURE).sized(1.0f, 1.0f).build(new ResourceLocation(Reference.MOD_ID, "dark_latex_snow_leopard").toString()));
+    public static final RegistryObject<EntityType<DarkLatexSnowLeopardEntity>> DARK_LATEX_SNOW_LEOPARD = ENTITY_TYPES.register("dark_latex_snow_leopard", () -> EntityType.Builder.of(DarkLatexSnowLeopardEntity::new, EntityClassification.CREATURE).sized(1.0f, 2.0f).build(new ResourceLocation(Reference.MOD_ID, "dark_latex_snow_leopard").toString()));
 }
