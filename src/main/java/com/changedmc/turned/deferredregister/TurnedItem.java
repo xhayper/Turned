f04751117a4c536a2fd948ad62157d4c2f0106cd
Alexandr Drozdov <avdrozdov_2@edu.hse.ru>
2021-10-29 15:41:33 +0300

    Fixed naming problems Added DarkLatexPickaxeItem

--- src/main/java/com/changedmc/turned/deferredregister/TurnedItem.java
+++ src/main/java/com/changedmc/turned/deferredregister/TurnedItem.java
@@ -1,7 +1,8 @@
 package com.changedmc.turned.deferredregister;
 
 import com.changedmc.turned.item.BatonItem;
-import com.changedmc.turned.item.DLAxe;
+import com.changedmc.turned.item.DarkLatexAxeItem;
+import com.changedmc.turned.item.DarkLatexPickaxeItem;
 import com.changedmc.turned.util.Reference;
 import com.changedmc.turned.util.TurnedItemGroup;
 import net.minecraft.item.BlockItem;
@@ -14,7 +15,9 @@ public class TurnedItem {
     public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Reference.MOD_ID);
 
     public static final RegistryObject<Item> BATON = ITEMS.register("baton", BatonItem::new);
-    public static final RegistryObject<Item> DARK_LATEX_AXE = ITEMS.register("dark_latex_axe", DLAxe::new);
+    public static final RegistryObject<Item> DARK_LATEX_AXE = ITEMS.register("dark_latex_axe", DarkLatexAxeItem::new);
+    public static final RegistryObject<Item> DARK_LATEX_PICKAXEAXE = ITEMS.register("dark_latex_pickaxe", DarkLatexPickaxeItem::new);
+
 
     public static final RegistryObject<Item> DARK_LATEX_BLOCK = ITEMS.register("dark_latex", () -> new BlockItem(TurnedBlock.DARK_LATEX_BLOCK.get(), new Item.Properties().tab(TurnedItemGroup.Utility)));
 
---
+++ src/main/java/com/changedmc/turned/item/DarkLatexPickaxeItem.java
@@ -0,0 +1,21 @@
+package com.changedmc.turned.item;
+
+import com.changedmc.turned.util.TurnedItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.Rarity;
+
+public class DarkLatexPickaxeItem extends PickaxeItem {
+    public DarkLatexPickaxeItem() {
+        super(
+            TurnedItemTier.DARK_LATEX,
+            2,
+            3f,
+            new Item.Properties()
+                .tab(TurnedItemGroup.Utility)
+                .rarity(Rarity.COMMON)
+                .durability(125)
+                .defaultDurability(125)
+        );
+    }
+}
