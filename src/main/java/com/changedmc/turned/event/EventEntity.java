c8c558de44b94b49b7cd087ceb4c4f62601267a0
BlockTheVillager <44810043+BlockTheVillager@users.noreply.github.com>
2021-10-31 14:27:01 -0500

    Basic check
    For canceling the renderplayerevent.pre, you'll want to use setCanceled

--- src/main/java/com/changedmc/turned/event/EventEntity.java
+++ src/main/java/com/changedmc/turned/event/EventEntity.java
@@ -2,15 +2,19 @@ package com.changedmc.turned.event;
 
 import com.changedmc.turned.Main;
 import com.changedmc.turned.capability.transfur.TransfurCapability;
+import com.changedmc.turned.client.render.latex.DarkLatexFoxRenderer;
 import com.changedmc.turned.config.CommonConfig;
 import com.changedmc.turned.networking.NetworkManager;
 import com.changedmc.turned.networking.packet.server.SyncTransfurCapability;
 import com.changedmc.turned.util.Reference;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -45,4 +49,16 @@ public class EventEntity {
                 Main.LOGGER.debug(event.getEntity() + "'s isTransfured: " + iTransfurCapability.isTransfured());
             });
     }
+        
+        @SubscribeEvent
+        public static void onRenderPlayerPre(RenderPlayerEvent.Pre event) {
+        	PlayerEntity p = event.getPlayer();
+        	if(p.getCapability(TransfurCapability.TRANSFUR_CAPABILITY).orElseThrow(null).getTransfurType() == 1) //placeholder value check, write a different check later
+        	{
+        		
+        
+            }
+        
+        }
+    
 }
