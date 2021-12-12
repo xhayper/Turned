567218970f97e45366f18dbfef0f74999ae28a52
xhayper <hayper1919@gmail.com>
2021-12-12 23:07:43 +0700

    Enderdragon Booba EasterEgg

--- src/main/java/com/changedmc/turned/event/EventRenderer.java
+++ src/main/java/com/changedmc/turned/event/EventRenderer.java
@@ -6,7 +6,6 @@ import com.changedmc.turned.util.Reference;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
-import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -16,6 +15,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class EventRenderer {
 
     private static final LatexIndicator latexIndicator = new LatexIndicator();
+    private static final ResourceLocation ENDER_DRAGON_BOOBA = new ResourceLocation(Reference.MOD_ID, "textures/gui/enderdragon_booba.png");
 
     @SubscribeEvent
     @OnlyIn(Dist.CLIENT)
@@ -24,23 +24,22 @@ public class EventRenderer {
         latexIndicator.render(event.getMatrixStack(), event.getWindow());
 
         ////////////////////////
-        if (!TurnedClientConfig.enderDragonBooba.get() || Minecraft.getInstance().options.hideGui) return;
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderTexture(0, new ResourceLocation(Reference.MOD_ID, "textures/gui/enderdragon_booba.png"));
-        GuiComponent.blit(
-                event.getMatrixStack(), // MatrixStack / PoseStack
-                (event.getWindow().getGuiScaledWidth()/2) - (86/2), // x
-                (event.getWindow().getGuiScaledHeight()/2) - (138/2), // y
-                85, // width
-                137, // height
-                0,  // uOffset
-                0, // vOffset
-                850, // uWidth
-                1364, // vHeight
-                850, // textureWidth
-                1364 // textureHeight
-        );
+        if (TurnedClientConfig.enderDragonBooba.get() && !Minecraft.getInstance().options.hideGui) {
+            RenderSystem.setShaderTexture(0, ENDER_DRAGON_BOOBA);
+            GuiComponent.blit(
+                    event.getMatrixStack(), // MatrixStack / PoseStack
+                    (event.getWindow().getGuiScaledWidth() / 2) - (86 / 2), // x
+                    (event.getWindow().getGuiScaledHeight() / 2) - (138 / 2), // y
+                    85, // width
+                    137, // height
+                    0,  // uOffset
+                    0, // vOffset
+                    850, // uWidth
+                    1364, // vHeight
+                    850, // textureWidth
+                    1364 // textureHeight
+            );
+        }
     }
 
 }
