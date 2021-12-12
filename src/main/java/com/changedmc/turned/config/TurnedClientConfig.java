492fa883b64e48e3f6d206c87d0c6c9227074068
xhayper <hayper1919@gmail.com>
2021-12-12 22:58:15 +0700

    Easter egg

--- src/main/java/com/changedmc/turned/config/TurnedClientConfig.java
+++ src/main/java/com/changedmc/turned/config/TurnedClientConfig.java
@@ -5,8 +5,10 @@ import net.minecraftforge.common.ForgeConfigSpec;
 public class TurnedClientConfig {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> enderDragonBooba;
 
     static {
+        enderDragonBooba = BUILDER.define("enderDragonBooba", false);
         SPEC = BUILDER.build();
     }
 }
--- src/main/java/com/changedmc/turned/event/EventRenderer.java
+++ src/main/java/com/changedmc/turned/event/EventRenderer.java
@@ -1,7 +1,13 @@
 package com.changedmc.turned.event;
 
 import com.changedmc.turned.client.gui.component.LatexIndicator;
+import com.changedmc.turned.config.TurnedClientConfig;
+import com.changedmc.turned.util.Reference;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -16,6 +22,25 @@ public class EventRenderer {
     public static void onRenderGameOverlay(RenderGameOverlayEvent.Pre event) {
         if (Minecraft.getInstance().player == null) return;
         latexIndicator.render(event.getMatrixStack(), event.getWindow());
+
+        ////////////////////////
+        if (!TurnedClientConfig.enderDragonBooba.get() || Minecraft.getInstance().options.hideGui) return;
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 0.0F);
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, new ResourceLocation(Reference.MOD_ID, "textures/gui/enderdragon_booba.png"));
+        GuiComponent.blit(
+                event.getMatrixStack(), // MatrixStack / PoseStack
+                (event.getWindow().getGuiScaledWidth()/2) - (86/2), // x
+                (event.getWindow().getGuiScaledHeight()/2) - (138/2), // y
+                85, // width
+                137, // height
+                0,  // uOffset
+                0, // vOffset
+                850, // uWidth
+                1364, // vHeight
+                850, // textureWidth
+                1364 // textureHeight
+        );
     }
 
 }
